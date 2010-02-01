66b5d60084b7c7260fce35777a9b2df227935214
Josh Krall <krall@JoshMacPro.local>
2010-02-01 17:27:55 -0600

    Fixed mongomapper gem config lines.

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'mongomapper'
+  config.gem 'mongo_mapper'
+  config.gem 'mongo_ext'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
